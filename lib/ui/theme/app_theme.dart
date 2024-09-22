4e1dfda9d450a0e6187f202da68ce7865842adcd
Alfredo <alfredobautista1@gmail.com>
2024-09-22 19:19:23 +0200

    new fonts

--- lib/ui/theme/app_theme.dart
+++ lib/ui/theme/app_theme.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 ThemeData buildMaterial3Theme() {
+  final gowunTextTheme = GoogleFonts.gowunBatangTextTheme(ThemeData(brightness: Brightness.dark).textTheme);
   return ThemeData(
     useMaterial3: true,
+    brightness: Brightness.dark,
     colorScheme: ColorScheme.fromSeed(
       seedColor: const Color(0xFF1B73E8),
       brightness: Brightness.dark,
       primary: const Color(0xFF1B73E8),
       secondary: const Color(0xFFC7667A),
+      secondaryContainer: const Color.fromARGB(255, 244, 19, 64),
     ),
+    textTheme: ThemeData(
+      brightness: Brightness.dark,
+    ).textTheme.copyWith(
+        displayLarge: gowunTextTheme.displayLarge,
+        displayMedium: gowunTextTheme.displayMedium,
+        displaySmall: gowunTextTheme.displaySmall,
+        headlineLarge: gowunTextTheme.headlineLarge,
+        headlineMedium: gowunTextTheme.headlineMedium,
+        headlineSmall: gowunTextTheme.headlineSmall),
   );
 }
